44b7f260e16dbea01b0883ce3f2b57c7114b1ade
ammulureddy1430 <38815450+ammulureddy1430@users.noreply.github.com>
2019-09-01 21:25:33 +0530

    Update Multithreading1.java

--- src/Multithreading1.java
+++ src/Multithreading1.java
@@ -1,7 +1,12 @@
 class Multithreading1 implements Runnable {
     @Override
     public void run() {
-        System.out.println("multithread testing example");
+        int i;
+        for(i=0;i<10;i++)
+            try{
+        System.out.println(Thread.currentThread()+"value"+i);
+            } catch(Interruptedexception e)
+                Thread.sleep(100);
     }
     public static void main(String args[])
     {
