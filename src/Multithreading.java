f530774daf37076dec74089e4b96d0f601f7fc0d
ammulu <ammulu.reddy1430@gmail.com>
2019-09-01 20:17:52 +0530

    test1

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -3,6 +3,7 @@ public class Multithreading extends Thread {
         int i;
         for (i = 0; i < 10; i++)
             System.out.println("value" + i);
+        System.out.println("value");
     }
 
     public static void main(String args[]) {
